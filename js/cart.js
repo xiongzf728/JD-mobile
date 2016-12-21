d1756b7822d3204abd5f195ec7b02687cbd34bdb
feng <561525994@qq.com>
2016-12-21 23:11:23 +0800

    修改

--- js/cart.js
+++ js/cart.js
@@ -1,44 +1,44 @@
-window.onload = function(){
+window.onload = function () {
     /*删除弹框*/
     deleteWin();
-}
-/*删除弹框*/
-function deleteWin(){
+  }
+  /*删除弹框*/
+function deleteWin() {
 
-    var jdWin = document.querySelector('.jd_win');
-    var jdWinBox = jdWin.querySelector('.jd_win_box');
+  var jdWin = document.querySelector('.jd_win');
+  var jdWinBox = jdWin.querySelector('.jd_win_box');
 
-    var deleteBtns = document.querySelectorAll('.option_delete');
+  var deleteBtns = document.querySelectorAll('.option_delete');
 
-    var up = null;
+  var up = null;
 
-    for(var i = 0 ; i < deleteBtns.length ; i++){
-        deleteBtns[i].onclick = function(){
-            /*1.点击删除按钮*/
-            jdWin.style.display = 'block';
-            jdWinBox.className = 'jd_win_box myBounceInDown';
+  for (var i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].onclick = function () {
+      /*1.点击删除按钮*/
+      jdWin.style.display = 'block';
+      jdWinBox.className = 'jd_win_box myBounceInDown';
 
-            up = this.querySelector('.delete_up');
+      up = this.querySelector('.delete_up');
 
-            up.style.webkitTransition = 'all 1s';
-            up.style.transition = 'all 1s';
+      up.style.webkitTransition = 'all 1s';
+      up.style.transition = 'all 1s';
 
-            up.style.webkitTransformOrigin = 'left bottom';
-            up.style.transformOrigin = 'left bottom';
+      up.style.webkitTransformOrigin = 'left bottom';
+      up.style.transformOrigin = 'left bottom';
 
-            up.style.webkitTransform = 'rotate(-30deg) translateY(2px)';
-            up.style.transform = 'rotate(-30deg) translateY(2px)';
+      up.style.webkitTransform = 'rotate(-30deg) translateY(2px)';
+      up.style.transform = 'rotate(-30deg) translateY(2px)';
 
-        }
     }
+  }
 
-    jdWinBox.querySelector('.cancel').onclick = function(){
-        jdWin.style.display = 'none';
+  jdWinBox.querySelector('.cancel').onclick = function () {
+    jdWin.style.display = 'none';
 
-        if(up){
-            up.style.webkitTransform = 'none';
-            up.style.transform = 'none';
-        }
+    if (up) {
+      up.style.webkitTransform = 'none';
+      up.style.transform = 'none';
     }
+  }
 
 }
--- js/common.js
+++ js/common.js
@@ -4,62 +4,50 @@
 window.jdm = {};
 
 /*封装一个兼容的transitionEnd事件的方法*/
-jdm.transitionEnd = function(dom,callback){
-    /*
-    * 1.给谁绑定   dom
-    * 2.处理什么逻辑  callback  方法块（）  调用
-    * */
+jdm.transitionEnd = function (dom, callback) {
 
-    if(dom && typeof dom == 'object'){
-        dom.addEventListener('webkitTransitionEnd',function(){
-            /*同一个逻辑*/
-            callback && callback();
-            /*if(callback){
-                callback();
-            }*/
-        });
-        dom.addEventListener('transitionEnd',function(){
-            /*同一个逻辑*/
-            callback && callback();
-        });
-    }
+  if (dom && typeof dom == 'object') {
+    dom.addEventListener('webkitTransitionEnd', function () {
+      callback && callback();
+    });
+    dom.addEventListener('transitionEnd', function () {
+      callback && callback();
+    });
+  }
 
 }
 
 /*封装一个tap方法*/
-jdm.tap = function(dom,callback){
+jdm.tap = function (dom, callback) {
 
-    if(dom && typeof dom == 'object') {
+  if (dom && typeof dom == 'object') {
 
-        /*在触发touchend的时候计算时间差 */
-        /*判断有没有触发touchmove */
+    /*在触发touchend的时候计算时间差 */
+    /*判断有没有触发touchmove */
 
-        var startTime = 0;
-        var isMove = false;
+    var startTime = 0;
+    var isMove = false;
 
-        dom.addEventListener('touchstart', function (e) {
-            /* 时间戳 */
-            startTime = Date.now();
-        });
+    dom.addEventListener('touchstart', function (e) {
+      /* 时间戳 */
+      startTime = Date.now();
+    });
 
-        dom.addEventListener('touchmove', function (e) {
-            isMove = true;
-        });
+    dom.addEventListener('touchmove', function (e) {
+      isMove = true;
+    });
 
-        dom.addEventListener('touchend', function (e) {
-         
-            if(!isMove && (Date.now()-startTime)<150){
-                /*处理触发了这个tap事件的逻辑*/
-                callback && callback(e);
-            }
+    dom.addEventListener('touchend', function (e) {
 
-            /*重置参数*/
-            isMove = false;
-            startTime = 0;
-        });
-    }
+      if (!isMove && (Date.now() - startTime) < 150) {
+        /*处理触发了这个tap事件的逻辑*/
+        callback && callback(e);
+      }
+
+      /*重置参数*/
+      isMove = false;
+      startTime = 0;
+    });
+  }
 
 }
-
-
-
